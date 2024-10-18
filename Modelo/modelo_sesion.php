a575958ed5528505c37500e1b462ef7e3fd61ddc
Angelo Ruben Huancapaza Ponce <Angeloponce25@gmail.com>
2024-10-18 14:21:16 -0500

    181024

--- Modelo/modelo_sesion.php
+++ Modelo/modelo_sesion.php
@@ -1,14 +1,15 @@
 <?php
-
+ include '../conexion.php';    
 
 function iniciarSesion($parametro)
 {
 
-    include '../conexion.php';    
+   
     $usuario = $parametro['usuario'];
     $password = md5($parametro['password']);
-    $consulta = "SELECT usuario, password FROM usuarios WHERE usuario ='$usuario' AND password ='$password'";
-    
+    $consulta = "SELECT usuario, password FROM usuarios 
+    WHERE usuario ='$usuario' AND password ='$password'";
+    echo $consulta;
 
 
 
